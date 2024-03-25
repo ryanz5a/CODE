4c31e3481283240985afab15a3773db957402ddf
Ryan <ryanzoucho5@gmail.com>
2024-03-24 23:13:18 -0400

    finding functions and learning arguments

---
+++ homework.py
@@ -0,0 +1,78 @@
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total  
+
+
+# print(multiply(1,2,3,5))
+
+
+# Ryan has 92 in Physics, 94 in Math, 96 Chemistry, 89 in Biology, 100 in Sociology. Display the average.
+
+
+# def grade_average(*numbers):
+#     total = 1
+#     for number in numbers:
+#          total += number
+#     return total / 5 
+
+# print(grade_average(92, 94, 96, 89, 100))
+
+# def multi_divi(*numbers):
+#      total = 1
+#      for number in numbers:
+#           total *= number
+#      return total /100
+
+# print(multi_divi(1, 3, 2, 10, 6))
+
+
+
+
+# def grocery_price(*groceries):
+#     total = 1
+    
+#     for groc in groceries:
+#         total += groc 
+#         # after tax
+#         aftertax = ((total * .0825) + total)
+
+#     return total, aftertax
+
+
+# print(grocery_price(3,5,7,9))
+
+
+
+
+
+# As a Software Engineer, you make $120k yearly.You contribute $3500 in 401K and you pay $3000 for medicare. 
+# At a 12% tax rate, what is your net salary?
+
+# def net_salary(networth):
+
+#     annual_salary = 120000
+#     contribution_401k = 3500
+#     medicare = 3000
+#     tax_rate = 0.12
+
+
+
+
+
+# def grade(*numbers):
+#     total = sum(numbers)
+#     average = total / 5
+#     return average
+
+# ryan_average = grade(92, 94, 96, 89, 100)
+# print("Ryan's average grade is:", ryan_average)
+
+
+# ** args
+# def save_users(**user):
+#      print(user)
+    #  print(user["age"])
+    #  print(user["age"])
+
